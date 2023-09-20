5d14cbcf2fe0453709fcca65bf081ffe1a55963e
Carracalla <Catastroph1c76@gmail.com>
2023-09-20 17:05:02 +0200

    first real commit

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 from phospho import Agent, Message
 from dotenv import load_dotenv
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
+
 from langchain.chat_models import ChatOpenAI
-import os 
+from langchain.utilities import SerpAPIWrapper
+from langchain.agents import Tool, initialize_agent, AgentType
+from langchain.schema import SystemMessage, HumanMessage
 
 # Load your environment variables
 load_dotenv()
 
-open_ai_key = os.getenv("OPEN_AI_KEY")
+# Agent Logic with Langchain 
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
 
-# Initialize the agent
+search = SerpAPIWrapper()
+tools = [Tool(name="SERPAPI", func=search.run, description="Search the web")]
+
+search_agent = initialize_agent(tools=tools, llm=llm, agent=AgentType.OPENAI_MULTI_FUNCTIONS, verbose=True)
+
+#print(agent.run("Hello, what is the weather in Paris ?"))
+
+# Agent Logic embedded in a function
+def my_agent(input):
+    messages = [
+        SystemMessage(content="You are an helpful assistant named phospho agent. You can help me to find information on the web"),
+        HumanMessage(content=input),
+    ]
+    
+    response=search_agent.run(messages)
+
+    return response
+
+# Package it into a phospho agent
 agent = Agent()
 
-def ph_agent(input : str):
-    response = f"John asked : {input} {open_ai_key}"
-    return response 
-
-# Define your routes
-# you don't have to implement both routes
-"""
-@agent.ask()
-def myask(message):
-    print(f"Your message was {message.content}, now I can do stuff in the background")
-"""
-
 @agent.chat()
-def mychat(message):
-    response = ph_agent(message.content)
+def my_chat(message):
+    # Use our agent logic to generate a response
+    response = my_agent(message)
+    # Return the response in a Message object
     return Message(response)
 
-# That's it, you're done!
+#print(my_chat("what is the weather in Paris ?"))
