d4134515e037f4a26b2153cc4e6320e21c80eedb
Carracalla <Catastroph1c76@gmail.com>
2023-10-03 15:05:40 +0200

    Updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from phospho import Agent, Message
+from phospho import Agent, Message, Client
 from dotenv import load_dotenv
 
 from langchain.chat_models import ChatOpenAI
@@ -8,6 +8,10 @@ from langchain.schema import SystemMessage, HumanMessage
 
 # Load your environment variables
 load_dotenv()
+
+# Create a phospho client
+client = Client()
+
 
 # Agent Logic with Langchain 
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
@@ -35,8 +39,14 @@ agent = Agent()
 
 @agent.chat()
 def my_chat(message):
+    session = client.sessions.create(data=None)
+    task = client.tasks.create(session_id=session.id, additional_input=None, data=None)
+    step = client.steps.create(task_id=task.id, input=message.content, name="search step", is_last=True)
     # Use our agent logic to generate a response
     response = my_agent(message.content)
+
+    step.update(output=response.content)
+    session.update(metadata={"flag": "success"})
     # Return the response in a Message object
     return Message(response)
 
